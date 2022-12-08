0c67ab62841ff493d53c7843f08c4e8ef4ce4f41
finn <finnjowett971108@gmail.com>
2022-12-08 13:31:44 -0800

    2

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../components/Search";
+
+const Homepage = () => {
+  const auth = "563492ad6f91700001000001b356ec47773c4b17a1f525520c20740a";
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default Homepage;
